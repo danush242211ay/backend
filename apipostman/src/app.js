8012dc278cdc27bef97e5b27f8cb804480dce396
danush242211ay <danush8747@gmail.com>
2026-06-19 11:05:24 +0530

    post,get,delete,patch

--- apipostman/src/app.js
+++ apipostman/src/app.js
@@ -24,4 +24,24 @@ app.get('/notes',(req,res)=>{
     })
 })
 
+app.delete('/notes/:id',(req,res)=>{
+    const index = req.params.id;
+
+    delete notes[index];
+
+    res.status(200).json({
+        message: "notes deleted successfully"
+    })
+})
+
+app.patch('/notes/:id',(req,res)=>{
+    const index = req.params.id;
+    const description = req.body.description;
+
+    notes[index].description = description;
+
+    res.status(200).json({
+        message:" notes updated successfully"
+    })
+})
 module.exports = app
