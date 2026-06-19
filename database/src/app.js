c40c744783e0bceb9b8a132a31eb7980d2274065
danush242211ay <danush8747@gmail.com>
2026-06-19 17:01:23 +0530

    mongodb-api

--- database/src/app.js
+++ database/src/app.js
@@ -1,5 +1,54 @@
 const express = require('express');
+const notemodel = require('./models/note.model')
 
 const app = express();
+app.use(express.json());
 
+app.post('/notes', async (req,res)=>{
+
+    const data = req.body;
+    await notemodel.create({
+        title:data.title,
+        description:data.description,
+    })
+
+    res.status(201).json({
+        message : "Data created successfully"
+    })
+})
+
+app.get('/notes', async(req,res)=>{
+
+    const notes = await notemodel.find();
+
+    res.status(201).json({
+    message : "notes fetched successfully",
+    notes : notes
+    })    
+})
+
+app.delete('/notes/:id', async(req,res)=>{
+
+    const id = req.params.id;
+
+    await notemodel.findOneAndDelete({
+        _id : id
+    })
+
+    res.status(200).json({
+        message : "note deleted successfully" 
+    })
+})
+
+app.patch('/notes/:id', async(req,res)=>{
+    const id = req.params.id;
+    const description = req.body.description;
+    await notemodel.findOneAndUpdate(
+        {_id: id},
+        {description : description})
+
+    res.status(200).json({
+        message : "Updated successfully"
+    })
+})
 module.exports = app;
